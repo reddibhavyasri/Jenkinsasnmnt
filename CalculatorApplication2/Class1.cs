114b7506e752c490d208f4736db58730e5063944
reddibhavyasri <reddibhavyasri@gmail.com>
2022-10-21 12:29:34 +0530

    push

---
+++ CalculatorApplication2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcu.atorApplication2
+{
+    public class Class1
+    {
+        public int add (int i,int j)
+        {
+            return i + j;
+        }
+        public int sub (int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
